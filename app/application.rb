1bc4ba71fd9f4e7c7378f23dbc95cafb50ca72d7
Joey Cho <cjy12182000@naver.com>
2018-08-22 03:32:44 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,8 +1,11 @@
+require 'pry'
+
 class Application
 
   def call(env)
     resp = Rack::Response.new
     some = Rack::NullLogger.new(self)
+    binding.pry
     if some.datetime_format <12
     resp.write "Good morning"
     else
